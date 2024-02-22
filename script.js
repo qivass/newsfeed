f2acd074f00ae705cb376c973c4024b0bc285a9f
alpolyanskiy <alpolyanskiy@ozon.ru>
2024-02-22 16:29:30 +0300

    add news

---
+++ script.js
@@ -0,0 +1,355 @@
+const data = {
+	"sources": [
+		{
+			"id": 1,
+			"name": "meduza"
+		},
+		{
+			"id": 6,
+			"name": "3dnews"
+		},
+		{
+			"id": 7,
+			"name": "nytimes"
+		},
+		{
+			"id": 8,
+			"name": "forbes"
+		},
+		{
+			"id": 9,
+			"name": "igromania"
+		},
+		{
+			"id": 10,
+			"name": "buro237"
+		},
+		{
+			"id": 11,
+			"name": "rusvesna"
+		},
+		{
+			"id": 12,
+			"name": "7ya"
+		},
+		{
+			"id": 14,
+			"name": "aif"
+		},
+		{
+			"id": 15,
+			"name": "gazetaru"
+		},
+		{
+			"id": 16,
+			"name": "karpov.courses"
+		}
+	],
+	"categories": [
+		{
+			"id": 1,
+			"name": "tech"
+		},
+		{
+			"id": 2,
+			"name": "sport"
+		},
+		{
+			"id": 3,
+			"name": "fashion"
+		},
+		{
+			"id": 4,
+			"name": "politics"
+		},
+		{
+			"id": 5,
+			"name": "other"
+		},
+		{
+			"id": 6,
+			"name": "karpov.courses"
+		}
+	],
+	"items": [
+		{
+			"id": 509889,
+			"lang": "ru",
+			"date": "Tue Feb 20 2024 16:36:23 GMT+0300 (Moscow Standard Time)",
+			"title": "Писатель Лосев назвал антиутопию самым популярным книжным жанром у россиян",
+			"description": "",
+			"image": "https://www.buro247.ru/images/drozdova2/878998099009909090.png",
+			"source_id": 14,
+			"category_id": 5
+		},
+		{
+			"id": 509896,
+			"lang": "ru",
+			"date": "Tue Feb 20 2024 16:36:18 GMT+0300 (Moscow Standard Time)",
+			"title": "Компания Apple стала крупнейшим плательщиком штрафов ФАС России в 2023 году",
+			"description": "Всего в бюджет поступило 5,8 миллиардов рублей антимонопольных взысканий.",
+			"image": "https://aif-s3.aif.ru/images/035/940/de9d290b5702b00b312172a5b42208d7.jpg",
+			"source_id": 14,
+			"category_id": 4
+		},
+		{
+			"id": 510089,
+			"lang": "ru",
+			"date": "Tue Feb 20 2024 16:35:42 GMT+0300 (Moscow Standard Time)",
+			"title": "Астрономы нашли самый яркий квазар во Вселенной",
+			"description": "Астрономы из Европейской южной обсерватории (ESO) в Германии обнаружили самый яркий квазар в космосе. Он расположен в 12 млрд световых лет от нашей планеты и сияет как 500 трлн солнц. Исследование опубликовано на официальном сайте научного ...",
+			"image": "https://img.gazeta.ru/files3/785/18305785/eso2402a-pic905-895x505-7182.jpg",
+			"source_id": 15,
+			"category_id": 1
+		},
+		{
+			"id": 509894,
+			"lang": "ru",
+			"date": "Tue Feb 20 2024 16:35:40 GMT+0300 (Moscow Standard Time)",
+			"title": "Авторы Everywhere из студии бывшего главы Rockstar North подтвердили увольнения",
+			"description": "",
+			"image": "https://newcdn.igromania.ru/articles/pics/tmp/images/2024/2/20/77c1f27d-aca7-4079-a2ad-ca6daa6c3572.jpg",
+			"source_id": 9,
+			"category_id": 1
+		},
+		{
+			"id": 509925,
+			"lang": "ru",
+			"date": "Tue Feb 20 2024 16:35:32 GMT+0300 (Moscow Standard Time)",
+			"title": "В Белом доме раскритиковали республиканцев за \"незаслуженный отпуск\" без помощи Украине",
+			"description": "Директор по связям с общественностью Белого дома Бен Лаболт заявил, что республиканцы в палате представителей США ушли на \"незаслуженный отпуск\", и их бездействие наносит ущерб нацбезопасности страны из-за успехов России на ...",
+			"image": "https://img.gazeta.ru/files3/784/13612784/AP06062707108-pic905-895x505-92381.jpg",
+			"source_id": 15,
+			"category_id": 4
+		},
+		{
+			"id": 509933,
+			"lang": "ru",
+			"date": "Tue Feb 20 2024 16:35:16 GMT+0300 (Moscow Standard Time)",
+			"title": "Африканские лидеры в 2024 году посетят Украину и Россию для продвижения мира",
+			"description": "Лидеры семи африканских стран - участниц мирной инициативы по Украине (Египет, Замбия, Коморские Острова, Сенегал, Конго, Уганда и Южно-Африканская Республика) намерены нанести визит на Украину и в Россию с целью продвижения мира. Об этом ...",
+			"image": "https://img.gazeta.ru/files3/617/17963617/RIA_8128330-pic905-895x505-76594.jpg",
+			"source_id": 15,
+			"category_id": 4
+		},
+		{
+			"id": 509903,
+			"lang": "ru",
+			"date": "Tue Feb 20 2024 16:35:16 GMT+0300 (Moscow Standard Time)",
+			"title": "В Киеве могут возобновить работу аэропорта Борисполь в течение трех недель",
+			"description": "",
+			"image": "",
+			"source_id": 14,
+			"category_id": 5
+		},
+		{
+			"id": 509906,
+			"lang": "ru",
+			"date": "Tue Feb 20 2024 16:34:14 GMT+0300 (Moscow Standard Time)",
+			"title": "Пасынок главкома ВСУ Сырского заявил о желании получить гражданство России",
+			"description": "Иван Сырский прибыл в генконсульство РФ в Австралии.",
+			"image": "https://aif-s3.aif.ru/images/035/940/7932c240a27eac705521ae009f071145.jpg",
+			"source_id": 14,
+			"category_id": 4
+		},
+		{
+			"id": 509998,
+			"lang": "ru",
+			"date": "Tue Feb 20 2024 16:34:02 GMT+0300 (Moscow Standard Time)",
+			"title": "Названы неожиданные последствия решения США по поставкам СПГ",
+			"description": "Приостановка выдачи новых контрактов на экспорт сжиженного природного газа (СПГ) властями США может повлиять на отношение Германии к \"Северному потоку — 2\", считает глава Минэнерго РФ Николай Шульгинов. Его цитирует ...",
+			"image": "https://img.gazeta.ru/files3/34/15358034/RIAN_6503789.HR-pic905-895x505-54433.jpg",
+			"source_id": 15,
+			"category_id": 4
+		},
+		{
+			"id": 510018,
+			"lang": "ru",
+			"date": "Tue Feb 20 2024 16:34:00 GMT+0300 (Moscow Standard Time)",
+			"title": "Портативную консоль Sony PlayStation Portal взломали и запустили эмулятор PSP",
+			"description": "В ноябре прошлого года Sony выпустила портативную консоль PlayStation Portal стоимостью $200, которая предназначена для потокового воспроизведения игр с PlayStation 5. Теперь же двум инженерам Google удалось взломать устройство и запустить на нём эмулятор PPSSPP, что позволило сыграть на PlayStation Portal в PSP-версию Grand Theft Auto 3 без использования Wi-Fi.",
+			"image": "https://3dnews.ru/assets/external/illustrations/2024/02/20/1100533/265.jpg",
+			"source_id": 6,
+			"category_id": 1
+		},
+		{
+			"id": 510001,
+			"lang": "ru",
+			"date": "Tue Feb 20 2024 16:33:02 GMT+0300 (Moscow Standard Time)",
+			"title": "Экономист предсказал новый рост цен на жилье в 2024 году",
+			"description": "Экономист Алексей Кричевский предсказал новый рост цен на жилье в 2024 году. Его слова приводит \"Федерал пресс\".\n\n\"Семейную ипотеку точно продлят, и на какой срок – неизвестно. Теоретически она может остаться и лет на ...",
+			"image": "https://img.gazeta.ru/files3/189/17713189/RIA_8387630-pic905-895x505-56702.jpg",
+			"source_id": 15,
+			"category_id": 4
+		},
+		{
+			"id": 509999,
+			"lang": "ru",
+			"date": "Tue Feb 20 2024 16:33:00 GMT+0300 (Moscow Standard Time)",
+			"title": "Need for Speed Unbound до конца года получит четыре обновления по мотивам предыдущих NFS",
+			"description": "Издательство Electronic Arts и студия Criterion Games объявили, что гоночная аркада с открытым миром Need for Speed Unbound будет получать контент на протяжении второго года после релиза. Соответствующее сообщение появилось в официальном микроблоге.",
+			"image": "https://3dnews.ru/assets/external/illustrations/2024/02/20/1100532/nfs_unbound.jpg",
+			"source_id": 6,
+			"category_id": 1
+		},
+		{
+			"id": 509913,
+			"lang": "ru",
+			"date": "Tue Feb 20 2024 16:32:25 GMT+0300 (Moscow Standard Time)",
+			"title": "Писатель Лосев назвал антиутопию самым популярным книжным жанром у россиян",
+			"description": "Также большим спросом пользуются книги по психологии и произведения в жанре фантастики.",
+			"image": "https://aif-s3.aif.ru/images/035/940/3c4ae8357e644f22121727e0161c355d.jpg",
+			"source_id": 14,
+			"category_id": 5
+		},
+		{
+			"id": 509891,
+			"lang": "ru",
+			"date": "Tue Feb 20 2024 16:30:39 GMT+0300 (Moscow Standard Time)",
+			"title": "«Может двигать курсором силой мысли»: Маск рассказал об успехах пациента с нейрочипом",
+			"description": "Первый человек, которому в мозг вживили нейрочип, смог силой мысли управлять движением компьютерной мыши, рассказал основатель Neuralink Илон Маск. Теперь компания работает над тем, чтобы пациент смог нажимать на кнопки мыши. Маск сообщил об успешной имплантации чипа Neuralink первому человеку в конце января. По его словам, устройство позволит его носителю, «просто подумав», управлять электронными устройствами",
+			"image": "",
+			"source_id": 8,
+			"category_id": 5
+		},
+		{
+			"id": 509895,
+			"lang": "ru",
+			"date": "Tue Feb 20 2024 16:29:46 GMT+0300 (Moscow Standard Time)",
+			"title": "Акционеры владельца сети «Мать и дитя» утвердили решения по перерегистрации в Россию",
+			"description": "Акционеры MDMG, владеющей сетью клиник «Мать и дитя», утвердили решения о переезде с Кипра в Россию на остров Октябрьский и конвертацию уставного капитала в рубли. Ранее глава компании Марк Курцер сообщал, что смена юрисдикции может завершиться весной и тогда MDMG вернется к дивидендам. Расписки MDMG на Мосбирже упали на 4%",
+			"image": "",
+			"source_id": 8,
+			"category_id": 5
+		},
+		{
+			"id": 509940,
+			"lang": "ru",
+			"date": "Tue Feb 20 2024 16:29:20 GMT+0300 (Moscow Standard Time)",
+			"title": "Лукашенко заявил, что не вакцинировался от COVID-19",
+			"description": "Президент Белоруссии Александр Лукашенко заявил, что не вакцинировался от COVID-19. Его слова приводит БелТА.\n\n\"Я сегодня прочитал абсурдную вещь: оказывается, во время пандемии для президента Беларуси был сформирован какой-то там ...",
+			"image": "https://img.gazeta.ru/files3/244/17056244/RIA_8406027-pic905-895x505-77388.jpg",
+			"source_id": 15,
+			"category_id": 4
+		},
+		{
+			"id": 509947,
+			"lang": "ru",
+			"date": "Tue Feb 20 2024 16:28:10 GMT+0300 (Moscow Standard Time)",
+			"title": "Раскрыт смысл подарка Путина лидеру Северной Кореи",
+			"description": "Президент Владимир Путин не просто так подарил лидеру КНДР Ким Чен Ыну лимузин Aurus российского производства. Этот автомобиль демонстрирует технологические возможности России, считает независимый автоэксперт Дмитрий Попов. Это машина ...",
+			"image": "https://img.gazeta.ru/files3/999/18304999/Untitled-1-pic905-895x505-71164.jpg",
+			"source_id": 15,
+			"category_id": 4
+		},
+		{
+			"id": 509921,
+			"lang": "ru",
+			"date": "Tue Feb 20 2024 16:28:02 GMT+0300 (Moscow Standard Time)",
+			"title": "В РАН связали окрашивание приморского льда в красный цвет с моллюсками",
+			"description": "Окраска прибрежных льдов в Приморском крае вызвана вмерзшими в лед моллюсками.",
+			"image": "https://aif-s3.aif.ru/images/035/939/62023b4bae3b212e1bbd3e7876e2562b.jpg",
+			"source_id": 14,
+			"category_id": 4
+		},
+		{
+			"id": 509929,
+			"lang": "ru",
+			"date": "Tue Feb 20 2024 16:27:51 GMT+0300 (Moscow Standard Time)",
+			"title": "Лера Кудрявцева назвала сроки проведения операции на колене",
+			"description": "Телеведущая Лера Кудрявцева поделилась, что на неделе ей проведут операцию на колене.",
+			"image": "https://aif-s3.aif.ru/images/035/940/cbb682e41c9c7a3ff21a940a34dc5282.png",
+			"source_id": 14,
+			"category_id": 5
+		},
+		{
+			"id": 509892,
+			"lang": "ru",
+			"date": "Tue Feb 20 2024 16:27:38 GMT+0300 (Moscow Standard Time)",
+			"title": "A$AP Rocky заявил, что Рианна работает над новым альбомом",
+			"description": "Известный хип-хоп-исполнитель A$AP Rocky рассказал поклонникам о том, что его возлюбленная Рианна работает над новым альбомом. Видео с музыкантом начало появляться в социальных сетях. Фанатам удалось пообщаться с рэпером в Париже. Пока музыкант фотографировался с ними на...",
+			"image": "https://www.buro247.ru/images/drozdova2/878998099009909090.png",
+			"source_id": 10,
+			"category_id": 3
+		}
+	]
+}
+
+const {items, categories, sources} = data
+
+function formatDate(date) {
+	return new Date(date).toLocaleDateString('ru-RU', {
+		day: '2-digit', month: 'long',
+	});
+}
+
+function getCategoryName(categoryId) {
+	return categories.find(({id}) => id === categoryId).name;
+}
+
+function getSourceName(sourceId) {
+	return sources.find(({id}) => id === sourceId).name;
+}
+
+function escapeString(str) {
+	const symbols = {
+		'&': '&amp;',
+		'<': '&lt',
+		'>': '&gt',
+	}
+
+	return str.replace(/[&<>]/g, (tag) => symbols[tag] || tag);
+}
+
+function createMainNewsItem(item) {
+
+	return (`
+	<article class="main-article">
+		<div class="main-article__image-container">
+			<img class="main-article__image" src="${encodeURI(item.image)}" alt="Фото новости"/>
+		</div>
+		<div class="main-article__content">	
+			<span class="article__category main-article__category">${getCategoryName(item.category_id)}</span>
+			<h2 class="main-article__title">${escapeString(item.title)}</h2>
+			<p class="main-article__text">${escapeString(item.description)}</p>
+			<span class="article__source main-article__source">${getSourceName(item.source_id)}</span>
+		</div>
+	</article>
+`)
+}
+
+
+function createAsideNewsItem(item) {
+	return (`
+		<article class="aside-article">
+			<h3 class="aside-article__title">${escapeString(item.title)}</h3>
+			<p class="aside-article__info">
+				<span class="article__date aside-article__date">${formatDate(item.date)}</span>
+				<span class="article__source aside-article__source">${getSourceName(item.source_id)}</span>
+			</p>
+		</article>
+	`)
+}
+
+function main() {
+	const mainNews = items.slice(0, 3);
+	const asideNews = items.slice(3, 12);
+
+	const mainNewsContainer = document.querySelector('.articles__main');
+	const asideNewsContainer = document.querySelector('.articles__aside');
+	const template = document.createElement('template');
+
+	mainNews.forEach(item => {
+		template.innerHTML = createMainNewsItem(item);
+		mainNewsContainer.appendChild(template.content);
+		template.innerHTML = '';
+	})
+
+	asideNews.forEach(item => {
+		template.innerHTML = createAsideNewsItem(item);
+		asideNewsContainer.appendChild(template.content);
+		template.innerHTML = '';
+	});
+}
+
+main();
